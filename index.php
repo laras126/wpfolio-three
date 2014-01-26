1edd9869bd237cd3837b31cab582595d32b71713
laras126 <laras126@gmail.com>
2014-01-26 11:27:44 -0500

    stick style

--- index.php
+++ index.php
@@ -19,10 +19,10 @@
 								<?php bones_page_navi(); ?>
 						<?php } else { ?>
 								<nav class="wp-prev-next">
-										<ul class="clearfix">
-											<li class="prev-link"><?php next_posts_link(__('&laquo; Older Entries', "bonestheme")) ?></li>
-											<li class="next-link"><?php previous_posts_link(__('Newer Entries &raquo;', "bonestheme")) ?></li>
-										</ul>
+									<ul class="clearfix">
+										<li class="prev-link"><?php next_posts_link(__('&laquo; Older Entries', "bonestheme")) ?></li>
+										<li class="next-link"><?php previous_posts_link(__('Newer Entries &raquo;', "bonestheme")) ?></li>
+									</ul>
 								</nav>
 						<?php } ?>
 
